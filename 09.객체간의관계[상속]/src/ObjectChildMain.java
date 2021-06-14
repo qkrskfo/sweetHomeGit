eeb3d98dd9d0669b3bfabd3568ed96b2f118547e
narae <iampnr91@gmail.com>
2021-06-14 15:48:06 +0900

    0614

--- 09.객체간의관계[상속]/src/ObjectChildMain.java
+++ 09.객체간의관계[상속]/src/ObjectChildMain.java
@@ -1,17 +1,28 @@
 
+/*
+<< Object >>
+Class Object is the root of the class hierarchy. 
+Every class has Object as a superclass. 
+All objects, including arrays, implement the methods of this class.
+*/
+
 class ObjectChild extends Object {
 	
 	/*
 	 * extend Object는 생략 가능
 	 */
 	
+	
 }
 
 public class ObjectChildMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ObjectChild oc = new ObjectChild();
+		int hashCode = oc.hashCode();
+		System.out.println(hashCode);
+		System.out.println(Integer.toHexString(hashCode));
 	}
 
 }
