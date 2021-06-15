a2595b901d0cfea5e74fa6926d42bf10ab8192a0
narae <iampnr91@gmail.com>
2021-06-15 11:46:27 +0900

    0615-1146

--- 98.REPORT/src/com/itwill04/array/LottoMain.java
+++ 98.REPORT/src/com/itwill04/array/LottoMain.java
@@ -8,6 +8,7 @@ public class LottoMain {
 		
 		for(int i = 0; i < lottoNumber.length; i++) {
 			lottoNumber[i] = (int)(Math.random()*45)+1;
+			
 		}
 		for (int i = 0; i < lottoNumber.length; i++) {
 			System.out.print(lottoNumber[i]+" ");
