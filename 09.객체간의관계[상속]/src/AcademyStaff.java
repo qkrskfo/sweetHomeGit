99105eea8cb58414f16be43887119dd8d3ce84a9
narae <iampnr91@gmail.com>
2021-06-15 13:50:28 +0900

    0615-1350

---
+++ 09.객체간의관계[상속]/src/AcademyStaff.java
@@ -0,0 +1,16 @@
+
+public class AcademyStaff extends AcademyMember {
+
+	public String depart;
+	public AcademyStaff() {
+		// TODO Auto-generated constructor stub
+	}
+	public AcademyStaff(int no, String name,String depart) {
+		this.no = no;
+		this.name = name;
+		this.depart = depart;
+	}
+	
+	
+	
+}
