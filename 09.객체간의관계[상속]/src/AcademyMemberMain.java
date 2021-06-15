1e78db3aea8b65100c3d50b97b802e80a86de10c
narae <iampnr91@gmail.com>
2021-06-15 14:50:37 +0900

    09

--- 09.객체간의관계[상속]/src/AcademyMemberMain.java
+++ 09.객체간의관계[상속]/src/AcademyMemberMain.java
@@ -15,6 +15,16 @@ public class AcademyMemberMain {
 		AcademyStaff sf1 = new AcademyStaff(6, "WON", "MARKETING");
 		AcademyStaff sf2 = new AcademyStaff(7, "HAM", "ACCOUNTING");
 		
+		/*
+		st1.print(); 
+		st2.print(); 
+		st3.print(); 
+		gs1.print(); 
+		gs2.print(); 
+		sf1.print(); 
+		sf2.print(); 
+		 */
+		
 		System.out.println("-----------student------------");
 		st1.studentPrint();
 		st2.studentPrint();
---
+++ 10.객체간의관계[상속]-오버라이딩/src/AcademyGansa.java
@@ -0,0 +1,29 @@
+
+public class AcademyGansa extends AcademyMember {
+
+	public String subject;
+	public AcademyGansa() {
+		// TODO Auto-generated constructor stub
+	}
+	public AcademyGansa(int no, String name, String subject) {
+		this.no = no;
+		this.name = name;
+		this.subject = subject;
+	}
+	
+	public void gansaPrint() {
+		this.print();
+		System.out.println(this.subject);
+	}
+	
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+	
+}
