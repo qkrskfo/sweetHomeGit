6d1a61490c94cbe84be38098cb64a20305a1e057
iampnr91 <iampnr91@gmail.com>
2021-06-16 11:19:36 +0900

    0616-1119

--- 12.객체간의관계[상속]-객체형변환/src/AcademyMemberCastingMain.java
+++ 12.객체간의관계[상속]-객체형변환/src/AcademyMemberCastingMain.java
@@ -11,7 +11,14 @@ public class AcademyMemberCastingMain {
 		AcademyMember m5 = new AcademyStudent(5, "JANG", "CSS");
 		AcademyMember m6 = new AcademyStudent(6, "CHOI", "SALES");
 		AcademyMember m7 = new AcademyStudent(7, "PARK", "DEVELOPMENT");
-
+		
+		m1.print();
+		m2.print();
+		m3.print();
+		m4.print();
+		m5.print();
+		m6.print();
+		m7.print();
 	}
 
 }
