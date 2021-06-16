80713a9c06abd4b527a2bfc4616b0d7dc8e2422d
iampnr91 <iampnr91@gmail.com>
2021-06-16 13:39:47 +0900

    0616-1339

--- 12.객체간의관계[상속]-객체형변환/src/AcademyMemberCastingArrayMain.java
+++ 12.객체간의관계[상속]-객체형변환/src/AcademyMemberCastingArrayMain.java
@@ -29,6 +29,40 @@ public class AcademyMemberCastingArrayMain {
 		for (int i = 0; i < members.length; i++) {
 			members[i].print();
 		}
+
+		
+		System.out.println("--- instanceof ---");
+		/*
+		 * 연산자 --> instanceof
+		 *   - 형태 : 참조변수 instanceof 클래스이름
+		 *   - 연산결과(반환) : true or false (boolean 데이터)
+		 */
+		
+		AcademyMember am = new AcademyStudent(1, "김", "자바");
+		boolean isStudent = am instanceof AcademyStudent;
+		if(isStudent) {
+			System.out.println("학생객체--> "+am);
+			AcademyStudent tempStudent = (AcademyStudent)am; // 다시 캐스팅
+			System.out.println("학생반:"+tempStudent.getBan());
+		}
+		
+		System.out.println("------------AcademyMember 중에서 AcademyStudent 객체만 출력-------------");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i] instanceof AcademyStudent) {
+				members[i].print();	
+			}			
+		}
+		
+		System.out.println("------------AcademyMember 중에서 AcademyStudent 객체의 반을 출력------------");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i] instanceof AcademyStudent) {
+				members[i].print();	
+				AcademyStudent tempStudent = (AcademyStudent)members[i];
+				System.out.println(" >>> 학생의 반:"+tempStudent.getBan());
+			}			
+		}
+		
+				
 	}
 
 }
