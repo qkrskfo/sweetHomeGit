5cbb10b54007c890eb58ddcf8bf6d69be79dfe3d
narae <iampnr91@gmail.com>
2021-06-17 17:37:25 +0900

    0617-1737

--- 98.REPORT/src/com/itwill04/array/CarArrayMain.java
+++ 98.REPORT/src/com/itwill04/array/CarArrayMain.java
@@ -20,16 +20,83 @@ public class CarArrayMain {
 				carArray[i].print();
 			}
 		}
-		System.out.println("2.전체주차구획수");
-		System.out.println("3.주차가능주차구획수");
 		
+		System.out.println();
+		System.out.printf("2.전체주차구획수: ");
+		int count=0;
+		for (int i = 0; i < carArray.length; i++) {
+				count++;
+		}
+		System.out.println(count);
+		
+		
+		System.out.println();
+		System.out.print("3.주차가능주차구획수: ");
+		count=0;
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]==null) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		
+		System.out.println();
 		System.out.println("4.입차");
 		/*
 		 * 1.차량객체생성
 		 * 2.빈자리찾아서대입 // null을 찾아내서 넣어줘야지
 		 */
+		
+		
+		Car carIpcha1 = new Car("9024",14);
+		Car carIpcha2 = new Car("8888",16);
+		// 여러대를 하려면 parking 매니저..가 필요해..흠
+		
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]==null) {
+				carArray[i] = carIpcha1;
+				break;
+			}
+		}
+		
+		System.out.print("9024 차량 입차확인 - 주차가능주차구획수: ");
+		count=0;
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]==null) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		
+		System.out.println("9024 차량 입차확인 - 전체 차량출력");
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]!=null) {
+				carArray[i].print();
+			}
+		}
+		
+		
+		System.out.println();
 		System.out.println("5.차량번호 7789번  차한대 정보출력");
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]!=null && carArray[i].getNo().equals("7789")) {
+				carArray[i].print();
+				break;
+			}
+		}
+		
+		System.out.println();
 		System.out.println("6.입차시간 10시이후 차량여러대 찾아서 정보출력");
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]!=null && carArray[i].getInTime()>=10) {
+				carArray[i].print();
+			}
+		}
+		
+		System.out.println();
 		System.out.println("7.2389번차량 12시 출차");
 		/*
 		 * 1. 2389번차량찾기
@@ -37,6 +104,26 @@ public class CarArrayMain {
 		 * 3. 영수증출력
 		 * 4. 주차에서 차량삭제
 		 */
+		
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]!=null && carArray[i].getNo().equals("2389")) {
+				carArray[i].setOutTime(12);
+				carArray[i].calculateFee();
+				carArray[i].print();
+				carArray[i] = null;
+				break;
+			}
+		}
+		System.out.println();
+		System.out.print("출차확인 - 주차가능주차구획수: ");
+		count=0;
+		for (int i = 0; i < carArray.length; i++) {
+			if(carArray[i]==null) {
+				count++;
+			}
+		}
+		System.out.println(count);
 		
 		
 	}
