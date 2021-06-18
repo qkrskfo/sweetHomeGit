e30b78b1810d7b1d622118f8473e786b4870b950
narae <iampnr91@gmail.com>
2021-06-18 17:29:37 +0900

    0618

--- 98.REPORT/src/com/itwill04/array/AcademyMemberArrayMain.java
+++ 98.REPORT/src/com/itwill04/array/AcademyMemberArrayMain.java
@@ -13,18 +13,86 @@ public class AcademyMemberArrayMain {
 				new AcademyGangsa(7, "DIM", "자바"),
 				new AcademyStaff(8, "AIM", "영업"),
 				new AcademyStaff(9, "QIM", "생산")
+
 		};
-		System.out.println("1.AcademyMember 전체출력");
+		System.out.println("1.AcademyMember 전체출력");		
+		for (int i = 0; i < members.length; i++) {
+			members[i].print();
+		}
 		System.out.println("2.번호 1   번 AcademyMember 한명 출력");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i].getNo()==1) {
+				members[i].print();
+				break;
+			}
+		}
 		System.out.println("2.번호 2   번 AcademyMember 한명 출력");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i].getNo()==2) {
+				members[i].print();
+				break;
+			}
+		}
 		System.out.println("2.이름 KIM 인 AcademyMember 들   출력");
-		System.out.println("3.AcademyMember 중에서 AcademyStudent들 출력");
-		System.out.println("3.AcademyMember 중에서 AcademyGangsa들 출력");
-		System.out.println("3.AcademyMember 중에서 AcademyStaff들 출력");
-		System.out.println("4.AcademyMember 중에서 자바 반  인 AcademyStudent 들 출력");
-		System.out.println("4.AcademyMember 중에서 영업 부서인 AcademyStaff   들 출력");
-		System.out.println("4.AcademyMember 중에서 자바 과목인 AcademyGangsa  들 출력");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i].getName().equals("KIM")) {
+				members[i].print();
+				break;
+			}
+		}
 		
+		/*
+		 * 연산자 --> instanceof
+		 *   - 형태 : 참조변수 instanceof 클래스이름
+		 *   - 연산결과(반환) : true or false (boolean 데이터)
+		 */
+		
+		System.out.println("3.AcademyMember 중에서 AcademyStudent들 출력");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i] instanceof AcademyStudent) {
+				members[i].print();
+			}
+		}
+		System.out.println("3.AcademyMember 중에서 AcademyGangsa들 출력");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i] instanceof AcademyGangsa) {
+				members[i].print();
+			}
+		}
+		System.out.println("3.AcademyMember 중에서 AcademyStaff들 출력");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i] instanceof AcademyStaff) {
+				members[i].print();
+			}
+		}
+		System.out.println("4.AcademyMember 중에서 자바 반  인 AcademyStudent 들 출력");		
+		for (int i = 0; i < members.length; i++) {			
+			if(members[i] instanceof AcademyStudent) {
+				AcademyStudent tempStudent = (AcademyStudent)members[i];
+				if(tempStudent.getBan().equals("자바")) {
+					members[i].print();
+				}				
+			}
+		}
+		
+		System.out.println("4.AcademyMember 중에서 영업 부서인 AcademyStaff   들 출력");		
+		for (int i = 0; i < members.length; i++) {
+			if(members[i] instanceof AcademyStaff) {
+				AcademyStaff tempStaff = (AcademyStaff)members[i];
+				if(tempStaff.getDepart().equals("영업")) {
+					members[i].print();
+				}
+			}
+		}
+		System.out.println("4.AcademyMember 중에서 자바 과목인 AcademyGangsa  들 출력");
+		for (int i = 0; i < members.length; i++) {
+			if(members[i] instanceof AcademyGangsa) {
+				AcademyGangsa tempGangsa = (AcademyGangsa)members[i];
+				if(tempGangsa.getSubject().equals("자바")) {
+					members[i].print();
+				}
+			}
+		}
 		
 	}
 
