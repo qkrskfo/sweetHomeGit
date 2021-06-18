7f77701946b8ea798b538e89373d484ca740bc22
narae <iampnr91@gmail.com>
2021-06-18 11:44:25 +0900

    0618

--- 14.인터페이스[interface]/src/InterfaceABImplMain.java
+++ 14.인터페이스[interface]/src/InterfaceABImplMain.java
@@ -26,8 +26,38 @@ public class InterfaceABImplMain {
 
 	public static void main(String[] args) {
 
+		System.out.println("InterfaceABImpl");
+		InterfaceABImpl abImpl = new InterfaceABImpl();
+		abImpl.method1();
+		abImpl.method2();
+		abImpl.method3();
+		abImpl.method4();
 		
+		// object, interfaceA, interfaceB 모두 다 부모니까 이게 가능.
+		// object는 클래스, 나머지는 인터페이스니까 다중상속 가능.
+		System.out.println("Object");
+		Object o = abImpl;
+		// 오브젝트 캐스팅은 여기선 의미가 없음. 메소드 호출이 안돼.
 
+		System.out.println("InterfaceA");
+		InterfaceA ia = abImpl;
+		ia.method1();
+		ia.method2();
+//		ia.method3(); ia는 method3, method4는 정의되어있지 않잖아. 
+		
+		System.out.println("InterfaceB");
+		InterfaceB ib = abImpl;
+		ib.method3();
+		ib.method4();
+		// ib에는 method1, method2가 없자낭
+		
+		System.out.println("----------InterfaceA 와 InterfaceB간의 캐스팅---------");
+		InterfaceA ia2 = new InterfaceABImpl();
+		ia.method1();
+		ia.method2();
+		InterfaceB ib2 = (InterfaceB)ia2;
+		ib2.method3();
+		ib2.method4();
 	}
 
 }
