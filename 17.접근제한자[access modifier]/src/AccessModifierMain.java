692072409fee3b7a5bb442e0dda093f0bb696098
narae <iampnr91@gmail.com>
2021-06-21 15:23:23 +0900

    0621-1523

---
+++ 17.접근제한자[access modifier]/src/AccessModifierMain.java
@@ -0,0 +1,24 @@
+
+/*
+ * 클래스의 접근을 제한하는 제한자
+ * 		1. 클래스의 접근제한
+ *      2. 클래스의 멤버(변수,메쏘드)의 접근제한
+ *      3. 접근제한자(public,protected,(default),private) 
+ *      	A.public    : 외부모든 클래스(객체)에서 접근가능
+ *          B.protected : 같은패키지에 있는 모든클래스(객체)에서 접근가능
+ *                        다른패키지에 있는 모든클래스(객체)에서 접근불가능
+ *                        다른패키지에 있는 상속관계에 있는 자식클래스에서 접근가능
+ *          c.(default) : 같은패키지에 있는 모든클래스(객체)에서 접근가능
+ *          			  다른패키지에 있는 모든클래스(객체)에서 접근 불가능
+ * 			d.private   : 외부 모든 클래스(객체)에서 접근 불가능
+ * 
+ */
+
+public class AccessModifierMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub0
+
+	}
+
+}
---
+++ 17.접근제한자[access modifier]/src/president/korea/YS.java
@@ -0,0 +1,23 @@
+package president.korea;
+
+public class YS {
+	public int member1;
+	protected int member2;
+	int member3;
+	private int member4;
+	
+	public void method1() {
+		System.out.println("YS.method1()");
+	}
+	
+	protected void method2() {
+		System.out.println("YS.method2()");
+	}
+
+	void method3() {
+		System.out.println("YS.method3()");
+	}
+	private void method4() {
+		System.out.println("YS.method4()");
+	}
+}
