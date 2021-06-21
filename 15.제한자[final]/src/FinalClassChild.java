5a1f6c78acc0a065af4cd2a3f91675bbd4de9fa9
narae <iampnr91@gmail.com>
2021-06-21 10:31:00 +0900

    0621

--- 15.제한자[final]/src/FinalClassChild.java
+++ 15.제한자[final]/src/FinalClassChild.java
@@ -1,4 +1,16 @@
 
 public class FinalClassChild extends FinalClass {
-
+	
+	// FinalClass.method1() 재정의 (ctrl + space바) 
+	@Override
+	public void method1() {
+		System.out.println("FinalClassChild에서 FinalClass.method1() 재정의(Override오버라이드)");
+	}
+	
+	/*
+	<< Cannot override the final method from FinalClass >>
+	public void method2() {
+		
+	}
+	*/
 }
