2fa6b96c961650c7234950d059e13a2219c2739c
narae <iampnr91@gmail.com>
2021-06-21 09:33:11 +0900

    0621

---
+++ 00.NARAE_STUDY/src/NaraeExam/Exam01.java
@@ -0,0 +1,20 @@
+package NaraeExam;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		
+		String s = null;
+		String str = null;
+		s = str;
+		
+		// String result = s.concat("100");
+		
+		"abc".equals(s);
+		
+		// s.equals("abc");
+		
+
+	}
+
+}
