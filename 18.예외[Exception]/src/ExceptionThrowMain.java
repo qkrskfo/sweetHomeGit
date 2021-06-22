c7c3f4dd3fd9702f22d7399b48b6db895e7a65c6
narae <iampnr91@gmail.com>
2021-06-22 14:30:09 +0900

    44

---
+++ 18.예외[Exception]/src/ExceptionThrowMain.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ExceptionThrowMain {
+	/*
+	public static void main(String[] args) throws ClassNotFoundException, UnknownHostException, IOException {
+
+		System.out.println("stmt1");
+		Class.forName("First"); // 클래스가 실제로 로딩됨
+		System.out.println("stmt2");
+		Socket socket = new Socket("www.naver.com", 80);
+		System.out.println("stmt3");
+		First first = new First();
+		first.method1();
+		System.out.println("stmt4 return");
+	}                                                                                                 
+	*/
+	public static void main(String[] args) throws Exception {
+		System.out.println("stmt1");
+		Class.forName("First"); // 클래스가 실제로 로딩됨
+		System.out.println("stmt2");
+		Socket socket = new Socket("www.naver.com", 80);
+		System.out.println("stmt3");
+		First first = new First();
+		// first.method1();
+		System.out.println("stmt4 return");
+	}   
+}
