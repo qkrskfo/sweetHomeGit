f579a2372a842d6b3cd57e0785e5206b59f8368e
narae <iampnr91@gmail.com>
2021-06-23 17:39:55 +0900

    0623-1739

--- 98.REPORT/src/com/itwill05/service/student/StudentServiceUIMain.java
+++ 98.REPORT/src/com/itwill05/service/student/StudentServiceUIMain.java
@@ -21,7 +21,8 @@ public class StudentServiceUIMain {
 			System.out.println("5. 이름으로 검색");
 			System.out.println("6. 학생총점으로 오름차순정렬");
 			System.out.println("7. 학생이름으로 오름차순정렬");
-			System.out.println("8. 종료");
+			System.out.println("8. 학생추가");
+			System.out.println("9. 종료");
 			System.out.println("----------------------------");
 			System.out.print("메뉴번호선택> ");
 			
@@ -61,6 +62,25 @@ public class StudentServiceUIMain {
 			}else if(menuNo==7) {
 				//7. 학생이름으로 오름차순정렬
 			}else if(menuNo==8) {
+				//8. 학생추가
+				System.out.print(" >>학생정보입력(번호 이름 국어 영어 수학: ");
+			String noStr = scanner.next();
+			String nameStr = scanner.next();
+			String korStr = scanner.next();
+			String engStr = scanner.next();
+			String mathStr = scanner.next();
+			System.out.println(noStr+", "+nameStr+", "+korStr+", "+engStr+", "+mathStr);
+			
+			int no = Integer.parseInt(noStr);
+			int kor = Integer.parseInt(korStr);
+			int eng = Integer.parseInt(engStr);
+			int math = Integer.parseInt(mathStr);
+			Student newStudent = new Student(no, nameStr, kor, eng, math);
+			
+			studentService.addStudent(newStudent); // 아직 구현 안되어있데
+			
+			
+			}else if(menuNo==9) {
 				run=false;
 				System.out.println("학생성적관리프로그램을 종료합니다.");
 			}
