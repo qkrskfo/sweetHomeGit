7ee91cb33ff0797b9f366367fd79877a4e9e1ef0
narae <iampnr91@gmail.com>
2021-06-24 11:11:33 +0900

    31

--- 19.컬렉션[collection]/src/HashMapMain.java
+++ 19.컬렉션[collection]/src/HashMapMain.java
@@ -14,9 +14,15 @@ public class HashMapMain {
 		carMap.put("4444", new Car("4444", 17));
 		carMap.put("5555", new Car("5555", 20));
 		System.out.println("map size: "+carMap.size());
+		System.out.println(carMap);
 		
-		
-		
+		/*
+		 * 동일한(equals) 키값은 존재하지 않음
+		 * 키객체가 동일(equals)하면 값객체가 바뀐다.
+		 */
+		carMap.put("3333", new Car("삼삼삼", 15));
+		System.out.println("map size: "+carMap.size());
+		System.out.println(carMap);
 	}
 
 }
