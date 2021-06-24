ab79d608e116d1aab1b93bf1ccc7763fc55a30e4
narae <iampnr91@gmail.com>
2021-06-24 11:10:25 +0900

    33

--- 19.컬렉션[collection]/src/HashMapMain.java
+++ 19.컬렉션[collection]/src/HashMapMain.java
@@ -1,8 +1,22 @@
+import java.util.HashMap;
 
 public class HashMapMain {
 
 	public static void main(String[] args) {
 
+		HashMap carMap = new HashMap();
+		System.out.println("map size: "+carMap.size());
+		System.out.println("---- 1. put ----");
+		Car c1 = new Car("1111", 12);
+		carMap.put("1111", 12);
+		carMap.put("2222", new Car("2222", 13));
+		carMap.put("3333", new Car("3333", 15));
+		carMap.put("4444", new Car("4444", 17));
+		carMap.put("5555", new Car("5555", 20));
+		System.out.println("map size: "+carMap.size());
+		
+		
+		
 	}
 
 }
