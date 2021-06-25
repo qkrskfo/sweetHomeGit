5393b4dd910d3acad0aa9dd4c8d652417cc78871
narae <iampnr91@gmail.com>
2021-06-25 11:37:36 +0900

    22

--- 19.컬렉션[collection]/src/generic/GenericArrayListMain.java
+++ 19.컬렉션[collection]/src/generic/GenericArrayListMain.java
@@ -1,6 +1,7 @@
 package generic;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class GenericArrayListMain {
 
@@ -52,6 +53,40 @@ public class GenericArrayListMain {
 		removeAccount.print();
 		System.out.println("#size: "+accountList.size());
 		System.out.println(accountList);
+		
+		System.out.println("================business method (업무실행)=================");
+		System.out.println("****************1.계좌전체출력******************");
+		for (int i = 0; i < accountList.size(); i++) {
+			accountList.get(i).print();
+			//ArrayListMain이랑 비교했을 때 훨씬 간단해짐.
+			/*
+			Account tempAccount = accountList.get(i);
+			tempAccount.print();
+			*/
+		}
+		
+		System.out.println("****************2.계좌번호 \"3333\" 1개 찾기********************");
+		System.out.println("****************3.계좌주 이름 KING 여러개 찾기******************");
+		System.out.println("****************4.계좌번호 4444 1개 삭제 ***********************");
+		System.out.println("****************5. 계좌주 이름 KING 계좌 여러개 삭제 [QUIZ] ****");
+		
+		System.out.println("-------iteration[전체출력]--------");
+		/*
+		ArrayListMain.java 에서 했던것
+		Iterator accountIterator = accountList.iterator();
+		while (accountIterator.hasNext()) {
+			Account tempAccount = (Account)accountIterator.next();
+			tempAccount.print();
+			
+		}
+		 */
+
+		// 캐스팅 필요없어짐
+		Iterator<Account> accountIterator = accountList.iterator();
+		while(accountIterator.hasNext()) {
+			Account tempAccount = accountIterator.next();
+			tempAccount.print();
+		}
 		
 	}
 
