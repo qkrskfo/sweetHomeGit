1d9b36f3140e6dda6f9f2ed2efa7e72e331f24f0
박나래 <84892223+qkrskfo@users.noreply.github.com>
2021-06-28 17:11:57 +0900

    Add files via upload

--- 22.내부클래스[Nested class]/src/OuterClass.java
+++ 22.내부클래스[Nested class]/src/OuterClass.java
@@ -14,9 +14,13 @@ public class OuterClass {
 	}
 	
 	/*
-	 * 인스턴스멤버 클래스
+	 * 인스턴스멤버 클래스[Nested class, Inner class, 내부클래스]
 	 */
 	public class InnerClass {
 		// 클래스라는 키워드가 붙으면 클래스가 생성이됨.
+		public int inner_member_field;
+		public void inner_member_method() {
+			System.out.println("InnerClass.inner_member_method()");
+		}
 	}
 }
