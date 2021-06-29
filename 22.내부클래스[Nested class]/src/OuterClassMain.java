6470930871595bdb6451bc369c8739068f430f4e
narae <iampnr91@gmail.com>
2021-06-29 09:39:56 +0900

    22

--- 22.내부클래스[Nested class]/src/OuterClassMain.java
+++ 22.내부클래스[Nested class]/src/OuterClassMain.java
@@ -5,8 +5,10 @@ public class OuterClassMain {
 		OuterClass oc = new OuterClass();
 		oc.outer_member_field = 325888;
 		oc.outer_member_method();
+		
+		//그러나 지들끼리 안에서 만들지 이렇게 밖에서 쓰진 않는다
 		OuterClass.InnerClass ic = oc.new InnerClass();
-
+		ic.inner_member_field = 3434;
 	}
 
 }
