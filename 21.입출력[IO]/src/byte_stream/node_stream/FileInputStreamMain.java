ed42b4afa4ffcaef5369384f7d8faaea07ae8663
narae <iampnr91@gmail.com>
2021-06-29 17:27:17 +0900

    1

--- 21.입출력[IO]/src/byte_stream/node_stream/FileInputStreamMain.java
+++ 21.입출력[IO]/src/byte_stream/node_stream/FileInputStreamMain.java
@@ -16,6 +16,20 @@ public class FileInputStreamMain {
 		
 		FileInputStream fis = new FileInputStream("fileOut.dat");
 		int readByte = fis.read();
+		
+	/*
+	 public abstract int read() throws IOException
+		- Reads the next byte of data from the input stream. 
+		- The value byte is returned as an int in the range 0 to 255. 
+		- If no byte is available because the end of the stream has been reached, the value -1 is returned. 
+		- This method blocks until input data is available, the end of the stream is detected, or an exception is thrown.
+		- A subclass must provide an implementation of this method.
+		
+		Returns: the next byte of data, or -1 if the end of the stream is reached.
+	 */
+		
+		
+		
 		System.out.println("1.byte: "+readByte);
 		System.out.println("1.byte: "+Integer.toBinaryString(readByte));
 		readByte = fis.read();
