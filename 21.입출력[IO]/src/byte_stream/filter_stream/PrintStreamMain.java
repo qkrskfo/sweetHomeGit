922205753a60b6e5159a99ab8200b33dcadc91af
narae <iampnr91@gmail.com>
2021-06-30 14:05:47 +0900

    1

--- 21.입출력[IO]/src/byte_stream/filter_stream/PrintStreamMain.java
+++ 21.입출력[IO]/src/byte_stream/filter_stream/PrintStreamMain.java
@@ -13,8 +13,50 @@ public class PrintStreamMain {
 
 		PrintStream ps = new PrintStream(new FileOutputStream("print.txt"));
 		
-		ps.write(65); // write는 필터링을 하지 않음
-		ps.write('A'); 
+		ps.write(65); // write는 필터링을 하지 않음 --> print.txt에 A가 나오네
+		ps.write('A');
+		
+		ps.print(65); // 필터링을 함. 보이는 그대로 문자로 나타나게 해줌. 
+		// --> print.txt에 65가 나옴.
+		ps.write('6');
+		ps.write('5');
+		// --> 이렇게해야 65가 나옴.
+		
+		ps.print(123456789);
+		ps.write('1');
+		ps.write('2');
+		ps.write('3');
+		ps.write('4');
+		ps.write('5');
+		ps.write('6');
+		ps.write('7');
+		ps.write('8');
+		ps.write('9');
+		
+		ps.print(true);
+		ps.write('t');
+		ps.write('r');
+		ps.write('u');
+		ps.write('e');
+		
+		ps.print(3.141592);
+		ps.write('3');
+		ps.write('.');
+		ps.write('1');
+		ps.write('4');
+		ps.write('1');
+		ps.write('5');
+		ps.write('9');
+		ps.write('2');
+		
+		ps.print(12345);
+		ps.write(49); // --> 1의 아스키코드가 49야
+		ps.write(50);
+		ps.write(51);
+		ps.write(52);
+		ps.write(53);
+		
+		System.out.println("--- PrintStream.print() --> print.txt ---");
 	}
 
 }
