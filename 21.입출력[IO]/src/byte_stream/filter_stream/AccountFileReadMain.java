42451485febcdec675a42089d58f7af1da43ecc1
narae <iampnr91@gmail.com>
2021-06-30 13:33:11 +0900

    11

--- 21.입출력[IO]/src/byte_stream/filter_stream/AccountFileReadMain.java
+++ 21.입출력[IO]/src/byte_stream/filter_stream/AccountFileReadMain.java
@@ -1,13 +1,24 @@
 package byte_stream.filter_stream;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
 public class AccountFileReadMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 
 		/*
-		 * 파일에서 Account데이터 read --> Account 객체 복원
+		 * 파일(accounts.ser)에서 Account데이터 read --> Account 객체 복원
 		 */
 		
+		DataInputStream dis = new DataInputStream(new FileInputStream("accounts.ser"));
+		Account account1 = new Account(dis.readInt(), dis.readUTF(), dis.readInt(), dis.readDouble());
+		Account account2 = new Account(dis.readInt(), dis.readUTF(), dis.readInt(), dis.readDouble());
+		
+		dis.close();
+		Account.headerPrint();
+		account1.print();
+		account2.print();
 	}
 
 }
