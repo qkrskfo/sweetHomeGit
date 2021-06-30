fb9a428f5e8ce3b678ce7fbabea7c2df68e60eed
narae <iampnr91@gmail.com>
2021-06-30 16:13:41 +0900

    33

--- 21.입출력[IO]/src/char_stream/FileReaderWriterMain.java
+++ 21.입출력[IO]/src/char_stream/FileReaderWriterMain.java
@@ -26,13 +26,34 @@ public class FileReaderWriterMain {
 		fw.close();
 		System.out.println("=== FileWriter.write() ===");
 		
+		/*
+		public int read() throws IOException
+		 - Reads a single character.
+		 - This method will block until a character is available, an I/O error occurs, or the end of the stream is reached.
+		 - Subclasses that intend to support efficient single-character input should overrids this method.
+		 Returns:
+		   The character read, as an integer in the range 0 to 65535 (0x00-0xffff),
+		    or -1 if the end of the stream has been reached.
+		 */
 		
 		FileReader fr = new FileReader("fileWriter.txt");
 		int readChar = fr.read();
-		System.out.println("1. readChar:"+readChar);
+		System.out.println("1. readChar:"+(char)readChar);
 		readChar = fr.read();
-		System.out.println("2. readChar:"+readChar);
+		System.out.println("2. readChar:"+(char)readChar);
 		
+		int lineCount=0;
+		while(true) {
+			readChar = fr.read();
+			if(readChar == -1) break;
+			System.out.print((char)readChar);
+			if(readChar == '\n') {
+				lineCount++;
+			}
+		}
+		fr.close();
+		System.out.println();
+		System.out.println("FileRead.read() --> "+lineCount+"라인 읽음");
 	}
 
 }
