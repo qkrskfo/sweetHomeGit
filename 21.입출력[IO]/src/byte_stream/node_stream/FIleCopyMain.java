c7eb9dd2830ab2693f436731ba4d503edce89dda
narae <iampnr91@gmail.com>
2021-06-30 09:34:00 +0900

    99

--- 21.입출력[IO]/src/byte_stream/node_stream/FIleCopyMain.java
+++ 21.입출력[IO]/src/byte_stream/node_stream/FIleCopyMain.java
@@ -11,6 +11,11 @@ public class FIleCopyMain {
 		FileOutputStream fos = new FileOutputStream("filebig_copy.lnk");
 		
 		int count = 0; // copy가 얼마나 됐는지 찍으려고
+		int starCount = 0;
+		
+		// 시간측정도 할 수 있엉
+		// 
+		long startTime = System.currentTimeMillis();
 		
 		while(true) {
 			int readByte = fis.read(); // 읽은 데이터를
@@ -19,12 +24,20 @@ public class FIleCopyMain {
 			count++; // 1byte를 썼다! 빵~
 			if(count%1024==0) {
 				System.out.print("★");
+				starCount++;
+				if(starCount%10==0) {
+					System.out.println();
+				}
 			}
 		}
+		long endTime = System.currentTimeMillis();
+		
 		fis.close();
 		fos.close(); // 빨대는 해제해주느게 좋아요
 		
+		System.out.println();
 		System.out.println(">> FileCopy: "+count+"bytes copied");
+		System.out.println(">> take "+(endTime - startTime/1000)+"seconds");
 	}
 
 }
