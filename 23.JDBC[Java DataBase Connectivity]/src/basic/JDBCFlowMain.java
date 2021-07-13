7f817756ec3e287a77bcc957117e7a41994a712f
narae <iampnr91@gmail.com>
2021-07-13 16:57:12 +0900

    777

--- 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
@@ -3,7 +3,28 @@ package basic;
 public class JDBCFlowMain {
 
 	public static void main(String[] args) {
-	
+		
+		String driverClass= "oracle.jdbc.OracleDriver";
+		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe"; // 선생님IP
+		String user = "javadeveloper2";
+		String password = "javadeveloper2";
+		
+		/*
+		 * 1. Driver Class loading 
+		 */
+		
+		/*
+		 * 2. Connection 객체 생성
+		 */
+			
+		/*
+		 * 3. Statement 객체 생성
+		 */
+			
+		/*
+		 * 4. SQL문 전송
+		 * 5. ResultSet 얻기
+		 */
 
 	}
 
