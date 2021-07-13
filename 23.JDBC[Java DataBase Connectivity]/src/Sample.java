c8b6c08ac6e97bb6a2cc4b990e671a50be0e5ff3
narae <iampnr91@gmail.com>
2021-07-13 16:34:26 +0900

    5

--- 23.JDBC[Java DataBase Connectivity]/src/Sample.java
+++ 23.JDBC[Java DataBase Connectivity]/src/Sample.java
@@ -1,3 +1,27 @@
+/*
+자바에서 데이터베이스에 연결!
+
+자바 애플리케이션에서 db에 액세스 가능
+
+DB vendor에 독립적(independent)이어야 함.
+
+JDBC API 
+ - (라이브러리 패키지) 제공
+
+JDK 
+ - jdbc api spec(interface) 제공
+
+Application Developer 자바 어플리케이션 개발자 님들은
+ - jdbc api 패키지 안의 라이브러리를 사용 db에 액세스 하기 위해
+ 
+DB Vendor (오라클, mySQL, db2, msSQL, postres 등)
+ - jdbc api spec(interface) 에 맞는 드라이버(구현(implement)클래스) 제공
+
+JDBC : 자바를 이용해 DB에 접근하여 각종 SQL문을 수행할 수 있도록 제공하는 API
+
+
+*/
+
 
 public class Sample {
 
