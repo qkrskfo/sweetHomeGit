d6d5732a8d4e1e0acf93869e72a317103cd7d9c6
narae <iampnr91@gmail.com>
2021-07-13 17:02:39 +0900

    87

--- 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
@@ -1,8 +1,11 @@
 package basic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class JDBCFlowMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		String driverClass= "oracle.jdbc.OracleDriver";
 		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe"; // 선생님IP
@@ -13,10 +16,16 @@ public class JDBCFlowMain {
 		 * 1. Driver Class loading 
 		 */
 		
+		Class.forName(driverClass);
+		System.out.println("1. Driver Class loading ");
+		
 		/*
-		 * 2. Connection 객체 생성
+		 * 2. Connection 객체 생성 (DB Server에 연결)
 		 */
-			
+		
+		Connection con = DriverManager.getConnection(url, user, password);
+		// java.sql 에 있는 인터페이스 Connection을 선택해야함.
+		System.out.println("2. Connection 객체 생성 (DB Server에 연결): "+con);
 		/*
 		 * 3. Statement 객체 생성
 		 */
