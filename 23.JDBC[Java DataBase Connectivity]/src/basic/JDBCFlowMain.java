71200abc69145a7a98dcddecf10c37a064b546ec
narae <iampnr91@gmail.com>
2021-07-13 17:25:01 +0900

    09

--- 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
@@ -2,6 +2,7 @@ package basic;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class JDBCFlowMain {
 
@@ -12,29 +13,38 @@ public class JDBCFlowMain {
 		String user = "javadeveloper2";
 		String password = "javadeveloper2";
 		
+		String selectSql = "select * from dept";
+		// 세미콜론 넣으면 안돼요 여기선~!
+		
 		/*
 		 * 1. Driver Class loading 
-		 */
-		
+		 */		
 		Class.forName(driverClass);
 		System.out.println("1. Driver Class loading ");
 		
+		
 		/*
 		 * 2. Connection 객체 생성 (DB Server에 연결)
-		 */
-		
+		 */		
 		Connection con = DriverManager.getConnection(url, user, password);
 		// java.sql 에 있는 인터페이스 Connection을 선택해야함.
 		System.out.println("2. Connection 객체 생성 (DB Server에 연결): "+con);
+		
+		
 		/*
-		 * 3. Statement 객체 생성
+		 * 3. Statement 객체 생성(SQL전송객체)
 		 */
-			
+		Statement stmt = con.createStatement();
+		// java.sql에 있는 인터페이스 Statement를 선택해야함.
+		System.out.println("3. Statement 객체 생성(SQL전송객체): "+stmt);
+		
+		
 		/*
 		 * 4. SQL문 전송
 		 * 5. ResultSet 얻기
 		 */
-
+		
+		stmt.executeQuery();
 	}
 
 }
