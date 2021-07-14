2257452a229589d6eb03d0e5db311ed194fd358a
narae <iampnr91@gmail.com>
2021-07-14 22:42:24 +0900

    q

--- 98.REPORT/src/com/itwill07/dao/phonebook/PhoneBook.java
+++ 98.REPORT/src/com/itwill07/dao/phonebook/PhoneBook.java
@@ -2,4 +2,45 @@ package com.itwill07.dao.phonebook;
 
 public class PhoneBook {
 
+	private int num;
+	private String name;
+	private String phone;
+	
+	public PhoneBook() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PhoneBook(int num, String name, String phone) {
+		super();
+		this.num = num;
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
 }
