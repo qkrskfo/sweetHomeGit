6fde026a743150700af7f3efe464c664d4f0480b
narae <iampnr91@gmail.com>
2021-07-14 10:36:01 +0900

    bb

--- 23.JDBC[Java DataBase Connectivity]/src/basic/driver/DriverLoadingMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/driver/DriverLoadingMain.java
@@ -1,14 +1,13 @@
 package basic.driver;
 
-public class OracleDriverLoadingMain {
+public class DriverLoadingMain {
 
 	public static void main(String[] args) throws Exception {
-
 		String driverClass1 = "basic.OracleDriver";
 		String driverClass2 = "basic.MySqlDriver";
 		Class.forName(driverClass1);
 		Class.forName(driverClass2);
-		
+
 	}
 
 }
