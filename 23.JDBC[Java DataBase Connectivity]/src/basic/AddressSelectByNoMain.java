1f43ee3622f20f1048dd166ca40359b1df4b224d
narae <iampnr91@gmail.com>
2021-07-14 11:52:09 +0900

    bb

--- 23.JDBC[Java DataBase Connectivity]/src/basic/AddressSelectByNoMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/AddressSelectByNoMain.java
@@ -4,26 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class AddressUpdateByNoMain {
+public class AddressSelectByNoMain {
 
-	public static void main(String[] args) throws Exception{
-		
+	public static void main(String[] args) throws Exception {
 		/**************DB 접속 정보*************/
 		String driverClass= "oracle.jdbc.OracleDriver";
 		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe";
 		String user = "javadeveloper2";
 		String password = "javadeveloper2";
-		/***************************/
+		/**************************************/
 
-		String updateSql = "update address set id='xxx',name='김경호',phone='899-9999',address='서울시 강남구' where no = 6";
+		String selectSql = "select no,id,name,phone,address from address where no = 6";
 		
 		
 		Class.forName(driverClass);
 		Connection con = DriverManager.getConnection(url, user, password);
 		Statement stmt = con.createStatement();
 		
-		int updateRowCount = stmt.executeUpdate(updateSql);
-		System.out.println(">> "+updateRowCount+"행 update");
+		stmt.executeQuery(selectSql);
+	
 
 	}
 
