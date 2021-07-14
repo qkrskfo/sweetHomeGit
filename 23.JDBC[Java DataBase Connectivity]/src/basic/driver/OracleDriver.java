4aa375d1b6305f9efe429964799418bb286366b7
narae <iampnr91@gmail.com>
2021-07-14 10:20:45 +0900

    5

--- 23.JDBC[Java DataBase Connectivity]/src/basic/driver/OracleDriver.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/driver/OracleDriver.java
@@ -1,4 +1,4 @@
-package basic;
+package basic.driver;
 
 import java.sql.Connection;
 import java.sql.Driver;
--- 23.JDBC[Java DataBase Connectivity]/src/basic/driver/StaticBlockClassMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/driver/StaticBlockClassMain.java
@@ -1,4 +1,4 @@
-package basic;
+package basic.driver;
 
 public class StaticBlockClassMain {
 
