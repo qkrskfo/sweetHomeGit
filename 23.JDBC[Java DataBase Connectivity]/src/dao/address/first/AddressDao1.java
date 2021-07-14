df3655819c89411aca7ecd6552920b0fd25ec38d
narae <iampnr91@gmail.com>
2021-07-14 14:17:40 +0900

    h

---
+++ 23.JDBC[Java DataBase Connectivity]/src/dao/address/first/AddressDao1.java
@@ -0,0 +1,34 @@
+package dao.address.first;
+
+/*
+Dao(Data Access Object)
+ - Address들의 데이터를 저장하고있는 Address테이블에
+   CRUD(Create, Read, Update, Delete) 작업을 할수있는
+   단위 메쏘드를 가지고있는 클래스
+
+ - AddressService객체 의 요청(메쏘드호출)을 받아서 
+   Data Access(File, DB)에 관련된 단위기능(CRUD)을
+   수행하는 객체
+ */
+
+
+public class AddressDao1 {
+
+	public void insert() throws Exception {
+		
+	}
+	
+	public void deleteByNo() throws Exception {
+		
+	}
+	
+	public void updateByNo() throws Exception {
+		
+	}
+	
+	public void selectByNo() throws Exception {
+		
+	}
+	
+	
+}
