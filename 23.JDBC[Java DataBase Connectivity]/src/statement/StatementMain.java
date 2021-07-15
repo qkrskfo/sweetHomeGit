dd91ba4ea85d8daf2e6fc0c8d99f98fe45dcd1c5
narae <iampnr91@gmail.com>
2021-07-15 11:52:33 +0900

    77

--- 23.JDBC[Java DataBase Connectivity]/src/statement/StatementMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/statement/StatementMain.java
@@ -1,13 +1,30 @@
 package statement;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class StatementMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		String driverClass= "oracle.jdbc.OracleDriver";
 		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe";
 		String user = "javadeveloper2";
 		String password = "javadeveloper2";
+		
+		Class.forName(driverClass);
+		Connection con = DriverManager.getConnection(url, user, password);
+		Statement stmt = con.createStatement();
+		
+		// Statement에 대해서 알아보려고~
+		
+		/*
+		내가 전송할 문장이 select인지 update인지 모를 땐 execute를 실행하면되지만, 추천하진 않음!
+		stmt.execute();
+		stmt.executeQuery();
+		stmt.executeUpdate();
+		*/
 		
 	}
 
