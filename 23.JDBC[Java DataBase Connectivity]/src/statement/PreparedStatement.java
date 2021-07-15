5702655255de0bcf0abfd7ab2f7fae1289025260
narae <iampnr91@gmail.com>
2021-07-15 14:28:44 +0900

    y

---
+++ 23.JDBC[Java DataBase Connectivity]/src/statement/PreparedStatement.java
@@ -0,0 +1,31 @@
+package statement;
+
+/*
+<<PreparedStatement>>
+	1. sql문작성
+	    - ?(파라메타) 를 사용해서 나중에 외부에서 데이타를 받을수있게한다.
+	      ex> insert into emp(empno,ename,job,manager,hiredate,sal,comm,deptno) values(?,?,?,?,?,?,?,?)	
+	    - 테이블이름,컬럼이름등은 ? 를 사용해서 외부에서 입력받을수없다.
+	      ex> select * from ? 
+	          select ?,? from emp    
+	2. PreparedStatement 객체생성시 인자로 sql문을 넣어서생성한다
+	   ex> PreparedStatement pstmt = con.prepareStatement(sql);
+	3. 생성된 PreparedStatement 객체에 파라메타를 setting 한다
+	    - sql 좌측 ? 부터 1,2,3...
+	   ex> 	pstmt.setInt(1,1234);              
+	 	   	pstmt.setString(2,'KIM');              
+	   		pstmt.setString(3,'MANAGER');              
+	  		pstmt.setInt(4,7839);              
+	   		pstmt.setDate(5,sysdate);              
+	   		pstmt.setDouble(6,1000.12);              
+	   		pstmt.setInt(7,0);              
+	   		pstmt.setInt(8,10);              
+	 4.  실행
+	    - 실행시 sql문을 인자로 넣지안는다.
+	    ex> pstmt.executeUpdate();         
+	        pstmt.executeQuery();         
+*/
+
+public class PreparedStatement {
+
+}
