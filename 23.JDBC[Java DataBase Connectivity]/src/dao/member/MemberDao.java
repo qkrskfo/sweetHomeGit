fc3dc6e36039d9b8c56bfcc909e5e53d963129de
narae <iampnr91@gmail.com>
2021-07-16 15:47:02 +0900

    4

--- 23.JDBC[Java DataBase Connectivity]/src/dao/member/MemberDao.java
+++ 23.JDBC[Java DataBase Connectivity]/src/dao/member/MemberDao.java
@@ -1,5 +1,7 @@
 package dao.member;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 import dao.common.DataSource;
@@ -26,26 +28,50 @@ public class MemberDao {
 	
 	
 	// 여기에는 반드시 DTO객체가 와야겠지!
+	// OR MAPPING 
 	public int insert(Member member) throws Exception {
+		String insertSql = "";
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(insertSql);
+		
 		return 0;
 	}
 	
 	// delete에도 Member객체를 써도됨. 아이디만 쓰면 되는거니깐.
 	public int deleteById(String m_id) throws Exception {
+		String deleteSql = "";
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(deleteSql);
+		
 		return 0;
 	}
 	
 	public int updateById(Member member) throws Exception {
+		String updateSql = "";
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(updateSql);
+		
 		return 0;
 	}
 	
 	public Member selectById(String m_id) throws Exception {
+		
+		String selectSql = "";
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(selectSql);
+		
+		// RO MAPPING : 관계형 DB 객체를 매핑한다고?
 		Member findMember = null;
 		
 		return findMember;
 	}
 	
 	public ArrayList<Member> selectAll() throws Exception {
+		
+		String selectAllSql = "";
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(selectAllSql);
+		
 		ArrayList<Member> memberList = new ArrayList<Member>();
 		
 		return memberList;
--- 23.JDBC[Java DataBase Connectivity]/src/dao/member/MemberDaoTestMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/dao/member/MemberDaoTestMain.java
@@ -5,7 +5,6 @@ public class MemberDaoTestMain {
 	public static void main(String[] args) throws Exception {
 
 		MemberDao memberDao = new MemberDao();
-		memberDao.insert(); // exception 던지세요~
 		
 	}
 
