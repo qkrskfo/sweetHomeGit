3cda250cc3a986f4d39059301503b9e944c87496
narae <iampnr91@gmail.com>
2021-07-26 16:20:41 +0900

    ..

---
+++ 23.JDBC[Java DataBase Connectivity]/src/dao/address/fourth/AddressService.java
@@ -0,0 +1,14 @@
+package dao.address.fourth;
+
+
+/*
+Service 객체
+ 	- 주소록 비즈니스로직(업무)을 수행하는 클래스
+ 	- Presentation객체(서블릿,JSP)에서 직접접근(메쏘드호출)하는 클래스
+ 	- AddressDao 객체를 멤버변수로 가지고 있음
+ 	- 주소록 업무수행 메소드에서 database접근(CRUD)이 필요하면 Dao를 사용한다.
+*/
+
+public class AddressService {
+
+}
