ff7d80fe1e0cfa5f568a38d05ebb411ec68ae2d6
narae <iampnr91@gmail.com>
2021-08-09 10:27:01 +0900

    ..

--- 00.erd(entity relation diagram)/src/com/itwill/shop/shop_table_create.sql
+++ 00.erd(entity relation diagram)/src/com/itwill/shop/shop_table_create.sql
@@ -1,4 +1,5 @@
-DROP TABLE TABLE_3 CASCADE CONSTRAINTS;
+DROP TABLE product CASCADE CONSTRAINTS;
+DROP TABLE board CASCADE CONSTRAINTS;
 DROP TABLE member_detail CASCADE CONSTRAINTS;
 DROP TABLE member CASCADE CONSTRAINTS;
 
@@ -12,12 +13,12 @@ CREATE TABLE member(
 
 CREATE TABLE member_detail(
 		m_id                          		VARCHAR2(10)		 NULL ,
-		md_carno                      		INTEGER(10)		 NULL ,
-		md_child                      		INTEGER(10)		 NULL 
+		md_carno                      		NUMBER(10)		 NULL ,
+		md_child                      		NUMBER(10)		 NULL 
 );
 
 
-CREATE TABLE TABLE_3(
+CREATE TABLE board(
 		board_no                      		NUMBER(3)		 NULL ,
 		board_title                   		VARCHAR2(100)		 NULL ,
 		board_content                 		VARCHAR2(512)		 NULL ,
@@ -27,12 +28,22 @@ CREATE TABLE TABLE_3(
 );
 
 
+CREATE TABLE product(
+		p_no                          		NUMBER(10)		 NULL ,
+		p_name                        		VARCHAR2(10)		 NULL ,
+		COLUMN_3                      		NUMBER(10)		 DEFAULT 0		 NULL ,
+		p_desc                        		VARCHAR2(256)		 NULL 
+);
+
+
 
 ALTER TABLE member ADD CONSTRAINT IDX_member_PK PRIMARY KEY (m_id);
 
 ALTER TABLE member_detail ADD CONSTRAINT IDX_member_detail_PK PRIMARY KEY (m_id);
 ALTER TABLE member_detail ADD CONSTRAINT IDX_member_detail_FK0 FOREIGN KEY (m_id) REFERENCES member (m_id);
 
-ALTER TABLE TABLE_3 ADD CONSTRAINT IDX_TABLE_3_PK PRIMARY KEY (board_no);
-ALTER TABLE TABLE_3 ADD CONSTRAINT IDX_TABLE_3_FK0 FOREIGN KEY (m_id) REFERENCES member (m_id);
+ALTER TABLE board ADD CONSTRAINT IDX_board_PK PRIMARY KEY (board_no);
+ALTER TABLE board ADD CONSTRAINT IDX_board_FK0 FOREIGN KEY (m_id) REFERENCES member (m_id);
+
+ALTER TABLE product ADD CONSTRAINT IDX_product_PK PRIMARY KEY (p_no);
 
